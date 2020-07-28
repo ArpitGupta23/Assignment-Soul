5c78a74bc526b94781e4e3d8e9ddaaeb7a35e7f1
ArpitGupta23 <66843153+ArpitGupta23@users.noreply.github.com>
2020-07-28 12:20:32 +0530

    Add files via upload

---
+++ Scripts/Copyright.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $("#messenger-copyrightsec").click(function () {
+        var win = window.open('http://m.me/112536423857236', '_blank');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+    });
+
+
+    $("#insta-copyrightsec").click(function () {
+        var win = window.open('https://www.instagram.com/assignmentsoul/', '_blank');
+            if (win) {
+                //Browser has allowed it to be opened
+                win.focus();
+            } else {
+                //Browser has blocked it
+                alert('Please allow popups for this website');
+            }
+        });
+
+    $("#fb-page-link").click(function () {
+        var win = window.open('https://www.facebook.com/Assignment-Soul-112536423857236/', '_blank');
+        if (win) {
+            //Browser has allowed it to be opened
+            win.focus();
+        } else {
+            //Browser has blocked it
+            alert('Please allow popups for this website');
+        }
+    });
+});
